b336f99cb13dbbaeb06403dee857ae85e4ad392c
James Doyley <jdoyley@gmail.com>
2016-04-11 09:01:06 +0100

    Split files into directories

--- shorty.rb
+++ shorty.rb
@@ -1,61 +1,28 @@
-require 'rubygems'
-require 'sinatra'
-require 'redis'
+class Shorty < Sinatra::Base
+  $redis = Redis.new host: 'localhost', port: 6379
 
-$redis = Redis.new host: 'localhost', port: 6379
-
-get '/' do
-  erb :index
-end
-
-post '/' do
-  if (params[:url] =~ URI::regexp).nil?
-    @error = 'Nah man, please enter a valid URL'
+  get '/' do
     erb :index
-  else
-    @token = (Time.now.to_i + rand(36**8)).to_s 36
-    $redis.set "links:#{ @token }", params[:url]
-    erb :shortened
+  end
+
+  post '/' do
+    if (params[:url] =~ URI::regexp).nil?
+      @error = 'Nah man, please enter a valid URL'
+      erb :index
+    else
+      @token = (Time.now.to_i + rand(36**8)).to_s 36
+      $redis.set "links:#{ @token }", params[:url]
+      erb :shortened
+    end
+  end
+
+  get '/:token/?' do
+    url = $redis.get "links:#{ params[:token] }"
+    if url.nil?
+      @error = "Sorry, ain't no URL there"
+    else
+      redirect url
+    end
   end
 end
-
-get '/:token/?' do
-  url = $redis.get "links:#{ params[:token] }"
-  if url.nil?
-    @error = "Sorry, ain't no URL there"
-  else
-    redirect url
-  end
-end
-
-__END__
-
-@@ layout
-<!doctype html>
-<html>
-  <head>
-    <title>Shorty</title>
-  </head>
-  <body>
-    <%= yield %>
-  </body>
-</html>
-
-@@ index
-<h1>Shorty</h1>
-<% if @error %>
-  <p>
-    <%= @error %>
-  </p>
-<% end %>
-<form action='/' method='post'>
-  <input type='url' name='url' size='40' placeholder='Enter a URL' />
-  <input type='submit' value='Shorten' />
-</form>
-
-@@ shortened
-<h1>Aw yeah shorty! Here's your link:</h1>
-<p>
-  <a href="http://localhost:4567/<%= @token %>">http://localhost:4567/<%= @token %></a>
-</p>
 
